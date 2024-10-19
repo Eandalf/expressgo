4f30269671b5d09ad4c3f2bd057d1e8704dda93e
Eandalf <clinah@connect.ust.hk>
2024-10-19 17:48:47 +0800

    chore: add mod tidy & clean to pipeline build

--- make.ps1
+++ make.ps1
@@ -1,8 +1,27 @@
+Write-Host "task: prepare"
+
+Write-Host "expressgo: tidy mod"
+go mod tidy -v
+
+Write-Host "goto: expressgo/examples/helloworld"
+Push-Location ".\examples\helloworld"
+
+Write-Host "expressgo/examples/helloworld: tidy mod"
+go mod tidy -v
+
+Write-Host "expressgo/examples/helloworld: clean"
+go clean -x
+
+Write-Host "goto: expressgo"
+Pop-Location
+
+Write-Host "task: build"
+
 Write-Host "expressgo: format"
 go fmt
 
 Write-Host "expressgo: install"
-go install
+go install -v
 
 Write-Host "goto: expressgo/examples/helloworld"
 Push-Location ".\examples\helloworld"
@@ -11,7 +30,7 @@ Write-Host "expressgo/examples/helloworld: format"
 go fmt
 
 Write-Host "expressgo/examples/helloworld: build"
-go build
+go build -v
 
 Write-Host "goto: expressgo"
 Pop-Location
